0943176b76a3b5470561924b6de88c80490581f2
Jiahao HAO <hjjhao@gmail.com>
2019-09-18 21:23:21 +1000

    add login components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LayoutComponent } from './layout/layout.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LayoutComponent } from "./layout/layout.component";
 
-import { CustomMaterialModule } from './material.module';
+import { CustomMaterialModule } from "./material.module";
+import { LoginComponent } from "./login/login.component";
+import { FormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LayoutComponent
-  ],
+  declarations: [AppComponent, LayoutComponent, LoginComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CustomMaterialModule
+    CustomMaterialModule,
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
